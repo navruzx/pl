bf8675903bf73097f0f16dca36cdb87a06fa62ba
navruzx <nnavruz095@gmail.com>
2024-07-26 16:01:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+// let matrex = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9],
+// ];
+// console.log(matrex[0][0]);
+// console.log(matrex[0][1]);
+// console.log(matrex[0][2]);
+// console.log(matrex[1][0]);
+// console.log(matrex[1][1]);
+// console.log(matrex[1][2]);
+// console.log(matrex[2][0]);
+// console.log(matrex[2][1]);
+// console.log(matrex[2][2]);
+// let eychtimel=html
+// let students = ["ali","zafar","oybek","sardor","dilshod"
+// ];
+// students[0] = "navruz";
+// students[1] = "shuhrat";
+// students[2] = "feruz";
+// students[3] = "doniyor";
+// students[4] = "olim";
+// console.log(students);
+// let a = [2,2,2,2,]
+// let numbers = [1, 2, 3, 4, 5];
+
+// // reversing the numbers array
+// let reversedArray = numbers.reverse();
+
+// console.log(reversedArray);
+
+// // // Output: [ 5, 4, 3, 2, 1 ]
+// let city = ["California", "Barcelona", "Paris", "Kathmandu"];
+
+// // sort the city array in ascending order
+// let sortedArray = city.sort();
+// console.log(sortedArray);
+
+// // Output: [ 'Barcelona', 'California', 'Kathmandu', 'Paris' ]
+// defining an array 
+// var fruits = ['Apple', 'Banana', 'Grape'];
+
+// // filling every element of the array with 'Cherry'
+// fruits.fill("Cherry");
+
+// console.log(fruits);
+
+// // Output: 
+// // [ 'Cherry', 'Cherry', 'Cherry' ]
+// let message = ["JavaScript", "is", "fun."];
+
+// // join all elements of array using space
+// let joinedMessage = message.join(" ");
+// console.log(joinedMessage);
+// // Output: JavaScript is fun.
+// // defining an array
+// let items = ["JavaScript", 1, "a", 3];
+
+// // returns a string with elements of the array separated by commas
+// let itemsString = items.toString();
+
+// console.log(itemsString);
+
+// // Output: 
+// // JavaScript,1,a,3
+// let cities = ["Madrid", "New York", "Kathmandu", "Paris"];
+
+// // remove the last element
+// let removedCity = cities.pop();
+
+// console.log(cities)         // ["Madrid", "New York", "Kathmandu"]
+// console.log(removedCity);   // Paris
+// let languages = ["English", "Java", "Python", "JavaScript"];
+
+// // removes the first element of the array
+// let first = languages.shift();
+// console.log(first);
+// console.log(languages);
+
+// // Output: English
+// //         [ 'Java', 'Python', 'JavaScript' ]
+// let primeNumbers = [2, 3, 5, 7]
+// let evenNumbers = [2, 4, 6, 8]
+
+// // join two arrays 
+// let joinedArrays = primeNumbers.concat(evenNumbers);
+// console.log(joinedArrays);
+
+// /* Output:
+// [
+//   2, 3, 5, 7,
+//   2, 4, 6, 8 
+// ]
+// */ 
+// let ism = ["ali","vali","olim"];
+// console.log(`Salom ${ism[0]} pleysteshinba meremmi`);
+// console.log(`Qaneysan ${ism[1]} kontrba meremmi`);
+// console.log(`Yaxshimsan ${ism[2]} basseynba meremmi`);
+// let sonlar = [2,3,5,5,5,6,54,2,42,4,24,2]
+// console.log(sonlar[0]+sonlar[1]+sonlar[2]+sonlar[3]+sonlar[4]+sonlar[5]+sonlar[6]+sonlar[7]+sonlar[8]+sonlar[9]+sonlar[10]+sonlar[11]);
+let t_shaxslar = ["Amir Temur","Alisher Navoiy","Adolf Gitler","Paygambarimiz (M.S.A.V)","Imom Husseyn (A.S)"]
+let z_shaxslar = ["Cristiano","Abdurahmon","Mahdi Rasooli","Saed Noori","Pouyanfar"]
+console.log( `Men tarixiy shaxslardan ${t_shaxslar[3]} bilan, zamonaviy shaxslardan esa ${z_shaxslar[2]} bilan korishishni istardim agar imkoniyat bo'lsa ` );
